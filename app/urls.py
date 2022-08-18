23ae7cbf7074fe4b9446360d79cd19cedce7dc25
Ashutosh <kesharvani99@gmail.com>
2022-08-18 16:22:42 +0530

    add urls and views

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-
+from .views import PictureListView, PictureDetailView
 
 app_name = 'farzicom'
 
 urlpatterns = [
-
+    path("cat/", PictureListView.asview(), name="Blog"),
+    path('cat/<int:pk>/', PictureDetailView.asview(), name="Blog Detail"),
 ]
--- app/views.py
+++ app/views.py
@@ -3,4 +3,39 @@ from rest_framework import generics
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework import permissions
 
+from .models import PicturesModel
+from .serializer import PictureSerializer
 
+
+class PictureListView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = PicturesModel.objects.all()
+    serializer_class = PictureSerializer
+    parser_classes = (MultiPartParser, FormParser,)
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class PictureDetailView(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin, 
+    mixins.DestroyModelMixin, 
+    generics.GenericAPIView
+):
+    queryset = PicturesModel.objects.all()
+    serializer_class = PictureSerializer
+    parser_classes = (MultiPartParser, FormParser,)
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
